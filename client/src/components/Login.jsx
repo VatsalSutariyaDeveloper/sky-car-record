014715ff6a644463dbdacaf17932242fe2875d34
YashSuvagiyaDeveloper <137169748+YashSuvagiyaDeveloper@users.noreply.github.com>
2023-10-02 17:29:30 +0530

    Changes into the Login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -28,6 +28,7 @@ const Login = () => {
                         src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
                         alt="Sample image" className='animate-login-image login-image' />
                 </div>
+                <h1>vatsal randi</h1>
                 <div className="md:w-1/3 max-w-sm">
                     <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded" type="text" placeholder="Email Address" />
                     <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="password" placeholder="Password" />
