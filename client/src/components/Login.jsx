8e7b590bd03b33459eb9abe37a88c8c4a4df7093
YashSuvagiyaDeveloper <137169748+YashSuvagiyaDeveloper@users.noreply.github.com>
2023-10-02 17:33:00 +0530

    "Vatsal r"

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -21,6 +21,7 @@ const Login = () => {
     };
 
     return (
+        
         <section className={`bg-primary ${styles.flexStart}`}>
             <div className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
                 <div className="md:w-1/3 md:-mt-[10px] max-w-sm -mt-36 lg:-mt-0">
