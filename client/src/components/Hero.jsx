3ca0392de4a96b39635dd6d963108d43cf44bc78
YashSuvagiyaDeveloper <137169748+YashSuvagiyaDeveloper@users.noreply.github.com>
2023-10-04 16:16:28 +0530

    "le vatsal bhikha"

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,11 +3,12 @@ import styles from '../style';
 import { date, deletebtn, edit, addcarbooking, datesearch, nodata } from '../assets';
 import '../index.css';
 import { Link } from 'react-router-dom';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const Hero = () => {
   const [bookings, setBookings] = useState([]);
   const [searchInput, setSearchInput] = useState('');
-
 
   useEffect(() => {
     fetch('http://localhost:3000/car-booking')
@@ -31,7 +32,40 @@ const Hero = () => {
     booking.carName.includes(searchInput)
   );
 
-  const deleteBooking = async (id) => {
+  const deleteBooking = (id) => {
+    iziToast.question({
+      timeout: false,
+      close: false,
+      overlay: true,
+      displayMode: 'once',
+      id: 'question',
+      zindex: 999,
+      title: 'Confirmation',
+      message: 'Are you sure you want to delete this user?',
+      position: 'center',
+      color: 'red',
+      buttons: [
+        [
+          '<button><b>Yes</b></button>',
+          function (instance, toast) {
+            instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+
+            // Perform the deletion after user confirmation
+            performDelete(id);
+          },
+          true,
+        ],
+        [
+          '<button>No</button>',
+          function (instance, toast) {
+            instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+          },
+        ],
+      ],
+    });
+  };
+
+  const performDelete = async (id) => {
     try {
       await fetch(`http://localhost:3000/car-booking/${id}`, {
         method: 'DELETE',
@@ -55,16 +89,19 @@ const Hero = () => {
             <div className={`${styles.flexCenter} flex-col xl:px-0 sm:px-16`}>
               <div className="flex items-center pb-6 ml-[255px]">
                 <div className="-ml-[270px] mx-2">
-                  <div className="p-[8px] lg:px-4 bg-black-gradient rounded-full lg:rounded-lg">
-                    <div className="flex items-center cursor-pointer" id="addbookingbtn">
-                      <img src={datesearch} alt="addcarbooking" className="w-7 lg:w-5" />
+                  <div className="p-[5px] lg:px-4 lg:py-[0.10rem] bg-black-gradient rounded-full lg:rounded-lg flex justify-center items-center">
+                    <div className="flex items-center cursor-pointer">
+                      <input
+                        className="border border-solid date-input mt-3"
+                        type="date"
+                      />
                       <span className="hidden lg:inline ml-2 text-white">Search Date</span>
-                      {/* <input type="date" id="customDateInput" /> */}
                     </div>
                   </div>
+
                 </div>
                 <input
-                  className="text-sm px-4 py-2 w-[280px] lg:w-[500px] border border-solid border-gray-300 rounded"
+                  className="text-sm px-3 py-2 w-[280px] lg:w-[500px] border border-solid border-gray-300 rounded"
                   type="search"
                   placeholder="Search"
                   value={searchInput}
@@ -98,10 +135,18 @@ const Hero = () => {
                                   src={deletebtn}
                                   alt="delete image"
                                   className="w-7 cursor-pointer hover:w-8"
-                                  onClick={() => deleteBooking(booking._id)} 
+                                  onClick={() => deleteBooking(booking._id)}
                                 />
                               </div>
                             </td>
+                          </tr>
+                          <tr>
+                            <td className="px-6 py-4 border-b border-gray-300">Client Name</td>
+                            <td className="px-6 py-4 border-b border-gray-300">{booking.clientName}</td>
+                          </tr>
+                          <tr>
+                            <td className="px-6 py-4 border-b border-gray-300">Dealer Name</td>
+                            <td className="px-6 py-4 border-b border-gray-300">{booking.dealerName}</td>
                           </tr>
                           <tr>
                             <td className="px-6 py-4 border-b border-gray-300">Car name</td>
@@ -114,14 +159,6 @@ const Hero = () => {
                           <tr>
                             <td className="px-6 py-4 border-b border-gray-300">Number Plate</td>
                             <td className="px-6 py-4 border-b border-gray-300">{booking.numberPlate}</td>
-                          </tr>
-                          <tr>
-                            <td className="px-6 py-4 border-b border-gray-300">Client Name</td>
-                            <td className="px-6 py-4 border-b border-gray-300">{booking.clientName}</td>
-                          </tr>
-                          <tr>
-                            <td className="px-6 py-4 border-b border-gray-300">Dealer Name</td>
-                            <td className="px-6 py-4 border-b border-gray-300">{booking.dealerName}</td>
                           </tr>
                           <tr>
                             <td className="px-6 py-4 border-b border-gray-300">Destination</td>
