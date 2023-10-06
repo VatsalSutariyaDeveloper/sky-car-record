d6e4f19a8bbbaae5f9256dbf083218e7c2c1717b
patelvivek1764 <patelvivek1764@gmail.com>
2023-10-06 15:21:57 +0530

    "final yo"

--- config/DatabseConnection.js
+++ config/DatabseConnection.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 const URL = process.env.DATABASE;
-mongoose.connect(URL)
+
+// Define options with useNewUrlParser set to true
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+  // Other connection options...
+};
+
+mongoose.connect(URL, options)
   .then(() => console.log('Connected!'))
   .catch(error => console.error('Connection error:', error));
