0ecbb05b8a6ea25e6d816c6958228b1850128717
patelvivek1764 <patelvivek1764@gmail.com>
2023-10-06 15:11:43 +0530

    "mango"

--- config/DatabseConnection.js
+++ config/DatabseConnection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URL = process.env().DATABASE;
+const URL = process.env.DATABASE;
 mongoose.connect(URL)
   .then(() => console.log('Connected!'))
   .catch(error => console.error('Connection error:', error));
